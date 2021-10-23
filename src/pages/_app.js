53cf96d3d55db8310969c00838f6b55d6afa4620
Marina <wearedusa@icloud.com>
2021-10-24 00:26:19 +0300

    Fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,32 +1,9 @@
 import Router from "next/router";
-import React from "react";
-import { YMInitializer } from "react-yandex-metrika";
+import withYM from "next-ym";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  React.useEffect(() => {
-    if (
-      typeof window !== "undefined" &&
-      process.env.NODE_ENV === "production"
-    ) {
-      const url = window.location.pathname + window.location.search;
-      ym("hit", url);
-    }
-  }, []);
-
-  return (
-    <>
-      {process.env.NODE_ENV === "production" && (
-        <YMInitializer
-          accounts={["86069306"]}
-          options={{ webvisor: true, defer: true }}
-          version="2"
-        />
-      )}
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
+export default withYM("86069306", Router)(MyApp);
